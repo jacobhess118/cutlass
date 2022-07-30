cee9a30c765deb858804dc74dcdf248b4875c0bb
jacobhess118 <jacobhess118@gmail.com>
2022-07-30 12:13:02 -0700

    up

--- examples/40_cutlass_py/test-cutlass-py.py
+++ examples/40_cutlass_py/test-cutlass-py.py
@@ -62,7 +62,7 @@ if err != cuda.CUresult.CUDA_SUCCESS:
 # Construct a module
 #
 
-cuda_install_path = sys.argv[2]
+cuda_install_path = sys.argv[1]
 architectures = [80,]
 include_paths = [
   '../../include',
