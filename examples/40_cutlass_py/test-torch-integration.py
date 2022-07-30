66a342c92508391c96bd40a3ff81cbcd5a6c6c15
jacobhess118 <jacobhess118@gmail.com>
2022-07-30 14:37:29 -0700

    up

--- examples/40_cutlass_py/test-torch-integration.py
+++ examples/40_cutlass_py/test-torch-integration.py
@@ -10,18 +10,18 @@ cuda_ver = "11.4"
 cuda_arch = "80"  # assuming A100
 
 manifest = cutlass_manifest.Manifest(args=SimpleNamespace(**dict(
-  operations="all",  # Specifies the operation to generate (gemm, all)
-  build_dir=".",  # CUTLASS top-level build directory
-  curr_build_dir=".",  # CUTLASS current build directory. cmake files will be emitted in this directory
-  generator_target="library",  # Target of CUTLASS Library Generator
-  architectures=cuda_arch,  # Target compute architectures, can be 53;60;61;70;75;80
-  kernels="",  # Comma delimited list to filter kernels by name
-  ignore_kernels="",  # Comma delimited list of kernels to exclude from build
-  filter_by_cc="True",  # If enabled, kernels whose comupte capability range is not satisfied by the build target are excluded
-  cuda_version=cuda_ver,  # Semantic version string of CUDA Toolkit
-  kernel_filter_file=None,  # Full path of filter file
-  selected_kernel_list=None,  # Specify the output log file containing all enabled kernels in this build
-  interface_dir=None,  # Interface header to kernels
+  operations="all",
+  build_dir=".",
+  curr_build_dir=".",
+  generator_target="library",
+  architectures=cuda_arch,
+  kernels="",
+  ignore_kernels="",
+  filter_by_cc="True",
+  cuda_version=cuda_ver,
+  kernel_filter_file=None,
+  selected_kernel_list=None,
+  interface_dir=None,
 )))
 generator.GenerateSM80_Simt_f32(manifest, None)
 
@@ -81,7 +81,3 @@ err = gemm.run(host_workspace, device_workspace, launch_config)
 
 if err != cuda.CUresult.CUDA_SUCCESS:
   raise RuntimeError('CUDA Error %s' % str(err))
-
-# Verify results
-if err != cuda.CUresult.CUDA_SUCCESS:
-  raise RuntimeError("CUDA Error %s" % str(err))
