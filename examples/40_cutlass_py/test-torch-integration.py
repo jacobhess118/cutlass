d2031d5bfe767df485a5c5c210372e393412764f
jacobhess118 <jacobhess118@gmail.com>
2022-07-30 14:35:08 -0700

    up

--- examples/40_cutlass_py/test-torch-integration.py
+++ examples/40_cutlass_py/test-torch-integration.py
@@ -1,12 +1,6 @@
 import torch
-
-# System modules
 from types import SimpleNamespace
-
-# CUDA Python modules
 from cuda import cuda
-
-# CUTLASS modules
 import library
 import manifest as cutlass_manifest
 import generator
@@ -14,10 +8,6 @@ import rt
 
 cuda_ver = "11.4"
 cuda_arch = "80"  # assuming A100
-
-#
-# Construct an SGEMM
-#
 
 manifest = cutlass_manifest.Manifest(args=SimpleNamespace(**dict(
   operations="all",  # Specifies the operation to generate (gemm, all)
@@ -35,23 +25,10 @@ manifest = cutlass_manifest.Manifest(args=SimpleNamespace(**dict(
 )))
 generator.GenerateSM80_Simt_f32(manifest, None)
 
-#
-# Look up the GEMM operation
-#
+operation = manifest.operations_by_name['cutlass_simt_sgemm_256x128_8x5_nn_align1']
 
-# # List all operations available
-# print(f"manifest.operations_by_name: {manifest.operations_by_name}")
-
-operation = manifest.operations_by_name['cutlass_simt_sgemm_256x128_8x5_nn_align1']  # TODO(yf225): should it be nn or nt?
-
-#
-# Construct a runtime GEMM operation
-#
 gemm = rt.Gemm(operation)
 
-#
-# Initialize context
-#
 err, = cuda.cuInit(0)
 
 if err != cuda.CUresult.CUDA_SUCCESS:
@@ -67,10 +44,6 @@ err, context = cuda.cuCtxCreate(0, device)
 if err != cuda.CUresult.CUDA_SUCCESS:
   raise RuntimeError("CUDA Error %s" % str(err))
 
-#
-# Construct a module
-#
-
 architectures = [int(cuda_arch),]
 include_paths = [
   f'../../../cutlass/include',
@@ -83,20 +56,15 @@ compilation_options = rt.CompilationOptions(architectures, include_paths)
 
 module = rt.Module('module.cu', [gemm], compilation_options)
 
-#
-# Setup a workspace
-#
-
-arguments = rt.GemmArguments()
-
 M, N, K = (3, 5, 4)
-arguments.problem_size = rt.GemmCoord(M, N, K)
 
-tensor_A_torch = torch.empty(3, 4, device='cuda', dtype=torch.float32)  # a
-tensor_B_torch = torch.empty(4, 5, device='cuda', dtype=torch.float32)  # b
-tensor_C_torch = torch.empty(3, 5, device='cuda', dtype=torch.float32)  # c
+tensor_A_torch = torch.empty(M, K, device='cuda', dtype=torch.float32)  # A
+tensor_B_torch = torch.empty(K, N, device='cuda', dtype=torch.float32)  # B
+tensor_C_torch = torch.empty(M, N, device='cuda', dtype=torch.float32)  # C
 tensor_D_torch = tensor_C_torch
 
+arguments = rt.GemmArguments()
+arguments.problem_size = rt.GemmCoord(M, N, K)
 arguments.A = rt.TensorRef(cuda.CUdeviceptr(tensor_A_torch.data_ptr()), M)
 arguments.B = rt.TensorRef(cuda.CUdeviceptr(tensor_B_torch.data_ptr()), N)
 arguments.C = rt.TensorRef(cuda.CUdeviceptr(tensor_C_torch.data_ptr()), M)
@@ -109,19 +77,11 @@ launch_config = gemm.plan(arguments)
 
 byte_count = gemm.initialize(host_workspace, device_workspace, launch_config, arguments)
 
-#
-# Launch the kernel
-#
-
 err = gemm.run(host_workspace, device_workspace, launch_config)
 
 if err != cuda.CUresult.CUDA_SUCCESS:
   raise RuntimeError('CUDA Error %s' % str(err))
 
-#
 # Verify results
-#
-# err, = cuda.cuStreamSynchronize(stream)
-
 if err != cuda.CUresult.CUDA_SUCCESS:
   raise RuntimeError("CUDA Error %s" % str(err))
