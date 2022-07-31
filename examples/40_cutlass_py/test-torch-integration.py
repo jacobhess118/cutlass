0e2ed3d376efd91df8f129a8c1a79102aabfb5d3
jacobhess118 <jacobhess118@gmail.com>
2022-07-30 17:10:29 -0700

    up

--- examples/40_cutlass_py/test-torch-integration.py
+++ examples/40_cutlass_py/test-torch-integration.py
@@ -1,5 +1,5 @@
 import torch
-import numpy as np
+import numpy as
 from types import SimpleNamespace
 from ctypes import c_void_p
 from cuda import cuda
@@ -60,11 +60,11 @@ module = rt.Module('module.cu', [gemm], compilation_options)
 
 M, N, K = (128, 512, 256)
 
-tensor_A_torch = torch.empty(M, K, device='cuda', dtype=torch.float32)  # A
-tensor_B_torch = torch.empty(K, N, device='cuda', dtype=torch.float32)  # B
-tensor_C_torch = torch.empty(M, N, device='cuda', dtype=torch.float32)  # C
-# tensor_D_torch = tensor_C_torch
-tensor_D_torch = torch.empty(M, N, device='cuda', dtype=torch.float32)  # D
+# tensor_A_torch = torch.empty(M, K, device='cuda', dtype=torch.float32)  # A
+# tensor_B_torch = torch.empty(K, N, device='cuda', dtype=torch.float32)  # B
+# tensor_C_torch = torch.empty(M, N, device='cuda', dtype=torch.float32)  # C
+# # tensor_D_torch = tensor_C_torch
+# tensor_D_torch = torch.empty(M, N, device='cuda', dtype=torch.float32)  # D
 
 # ======
 tensor_A = np.ndarray(M * K, dtype=np.float32)
