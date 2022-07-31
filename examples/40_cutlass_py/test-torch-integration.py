ca4a43ae5f15e8caebe378f6398be46665da82ca
jacobhess118 <jacobhess118@gmail.com>
2022-07-30 18:06:02 -0700

    up

--- examples/40_cutlass_py/test-torch-integration.py
+++ examples/40_cutlass_py/test-torch-integration.py
@@ -114,3 +114,5 @@ if err != cuda.CUresult.CUDA_SUCCESS:
 
 torch.cuda.synchronize()
 print(f"tensor_D_torch: {tensor_D_torch}")
+
+print(f"PyTorch result: {torch.mm(tensor_A_torch, tensor_B_torch) + tensor_C_torch}")
